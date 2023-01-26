110557db6e949aee10727dd03e60cc147c671f6e
Igor de Castro <igordecastro07@gmail.com>
2023-01-25 21:42:42 -0300

    pratica concluida

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,49 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  return prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+  return prisma.cars.findUnique({where: {
+    id
+  }});
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findUnique({
+    where: {
+      licensePlate 
+    }
+  })
+  //db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  return prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color
+    }
+  })
+}
+
+async function deleteCar(id: number) {
+  return prisma.cars.delete({
+    where: {
+      id
+    }
+  })
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
